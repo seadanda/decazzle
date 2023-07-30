e7edf60fff9e7c1ff9a7b86983c92de252d9cf96
Dónal Murray <donalm@seadanda.dev>
2023-07-30 17:58:54 +0100

    Add operator enum and allow a random operator to be chosen
    Implement Distribution trait for Standard distribution to generate a random
    value from the enum

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,5 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
+
+#[derive(Debug, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Distribution<Op> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Op {
+        match rng.gen_range(0..4) {
+            0 => Op::Add,
+            1 => Op::Sub,
+            2 => Op::Mul,
+            _ => Op::Div,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +25,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_generates_random_operations() {
+        let random: Op = rand::random();
+        assert!([Op::Add, Op::Sub, Op::Mul, Op::Div].contains(&random));
     }
 }
